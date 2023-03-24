1a3555dd80183a99ed0c4864872fe3c16be1ba5a
JRSkates <skatesproduction@gmail.com>
2023-03-24 14:24:25 +0000

    Bar Graph Updated

--- bar-graph.rb
+++ bar-graph.rb
@@ -1,5 +1,27 @@
+# Write a program that draws bar graphs.  It should:
+# * Ask the user for a list of numbers.  The user enters them
+#   separated by commas, e.g.: `10,4,2,7,5,1`.
+# * Print a horizontal bar graph of the numbers in order.
+#   * Bar should be made up of `-`s.
+#
+# * Example output from running the program:
+#   ```
+#   Enter some numbers to graph
+#   10,4,2,7,5,1
+#   ----------
+#   ----
+#   --
+#   -------
+#   -----
+#   -
+#   ```
+#
+# * Note: You can assume the user will only enter integers separated
+#   by commas with no spaces.
 
-user_input = "10,4,2,7,5,1"
+puts "Enter a list of numbers, separated by a comma"
+user_input = gets.chomp
+
 arr = user_input.split(",").map { |x| x.to_i }
 arr.each do |num|
   puts "-" * num
