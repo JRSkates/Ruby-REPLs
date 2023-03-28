624255a3ddbb4373a2c1bf9be1eec7d08666fd42
JRSkates <skatesproduction@gmail.com>
2023-03-28 18:23:31 +0100

    Hash Practice with detailed steps output

--- hash_pracitce_2.rb
+++ hash_pracitce_2.rb
@@ -9,20 +9,43 @@ people = [
 ]
 
 puts "Enter category to group people by"
-user_1 = gets.chomp
+user_input = gets.chomp
 
 sorted_by_input = {}
 
-people.each do |player|
-  input = player[user_1]
-  name = player["name"]
-  if sorted_by_input[input] == nil
-    sorted_by_input[input] = [name]
+people.each do |internal_hash|
+  puts
+  puts "LOOP START"
+  type_of_sport = internal_hash[user_input].capitalize
+  name = internal_hash["name"]
+  puts "The current final hash looks like: #{sorted_by_input}"
+  p type_of_sport
+  p name
+  if sorted_by_input[type_of_sport] == nil
+    puts "IF PATH START"
+    sorted_by_input[type_of_sport] = [name]
+    p sorted_by_input
+    puts "IF PATH END"
   else
-    sorted_by_input[input].push(name)
+    puts "ELSE PATH START"
+    sorted_by_input[type_of_sport].push(name)
+    p sorted_by_input
+    puts "ELSE PATH END"
   end
+  puts "LOOP END"
+  puts
 end
+
+puts "PEOPLE LOOP END"
+puts
+puts
+puts 
+p sorted_by_input
+puts
+puts "Here is a more readable list"
+puts
 sorted_by_input.each do |k, v|
-  puts k 
+  puts k + ":"
   puts v
+  puts
 end
