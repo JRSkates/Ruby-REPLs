e41e1a1cf5bd379c8db1bf131b0c1cc1909bb031
JRSkates <skatesproduction@gmail.com>
2023-03-29 13:35:56 +0100

    Refactored Averages

--- average-class-score.rb
+++ average-class-score.rb
@@ -13,9 +13,9 @@ test_scores_for_class_2 = [48, 99, 91, 70]
 test_scores_for_class_3 = [56, 58, 61, 98, 100]
 
 # Print out the averages
-puts "Class 1 average: " + average(test_scores_for_class_1).to_s
-puts "Class 2 average: " + average(test_scores_for_class_2).to_s
-puts "Class 3 average: " + average(test_scores_for_class_3).to_s
+puts "Class 1 average: #{average(test_scores_for_class_1).to_s}"
+puts "Class 2 average: #{average(test_scores_for_class_2).to_s}"
+puts "Class 3 average: #{average(test_scores_for_class_3).to_s}"
 
 # Print out the school average
 sch_average = average([average(test_scores_for_class_1), average(test_scores_for_class_2), average(test_scores_for_class_3)]).round(2).to_s
